6762ee9c65ea3dd51141dee1ecc540780be74165
SabahatSohail <65030081+SabahatSohail@users.noreply.github.com>
2020-06-14 19:26:54 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,280 @@
+//=========== Chapter1 Q1 ===================
+// alert("Hello Visitiors! Welcome to my website")
+
+//=========== Chapter1 Q2 ===================
+// alert("Error! Please enter a valid password")
+
+//=========== Chapter1 Q3 ===================
+// alert('Welcome to JS Land... \r\n Happy Coding! ')
+
+//=========== Chapter1 Q4 ===================
+// alert("Welcome to JS Land...")
+// alert("Happy Coding! \r\n Prevent this page from creating additional dialogs.")
+
+//=========== Chapter1 Q5 ===================
+
+//=========== Chapter1 Q6 ===================
+
+//=========== Chapter1 Q7 ===================
+
+//=========== Chapter2 Q1 ===================
+// var name = "Username";
+//  alert (name);
+
+//=========== Chapter2 Q2 ===================
+// var Myname = "Sabahat Sohail";
+// alert (Myname);
+
+//=========== Chapter2 Q3 ===================
+// var message = "Hello world"
+// alert (message);
+
+//=========== Chapter2 Q4 ===================
+// var name = "Johne Doe";
+// alert (name);
+// var age = "15 years old";
+// alert (age);
+// var qualification = "Certified Mobile Apllication Development"
+// alert (qualification);
+
+//=========== Chapter2 Q5 ===================
+// var a = "PIZZA" ;
+// var b = a.slice(0,4);
+// var c = a.slice(0,3);
+// var d = a.slice(0,2);
+// var e = a.slice(0,1);
+// var f = a + "\r\n" + b + "\r\n" + c +"\r\n" + d + "\r\n" + e;
+// alert (f);
+
+//=========== Chapter2 Q6 ===================
+// var email = " sabahatsohail96@gmail.com"
+// var a = "My email address is"
+// var c = a + email
+// alert (c)
+
+//=========== Chapter2 Q7 ===================
+// var book = " A smarter way to learn JavaScript";
+// var a = "I am trying to learn from the Book";
+// var c = a + book
+// alert (c)
+
+//=========== Chapter2 Q8 ===================
+// var a = "Yah! I can write HTML content through Javascript"
+// document.write(a)
+
+//=========== Chapter2 Q9 ===================
+// var a = " ▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬ "
+// alert (a)
+
+//=========== Chapter3 Q1 ===================
+// var age = 23
+// var a = "I am "
+// var b = " years old"
+// var c = a + age + b 
+// alert (c)
+
+//=========== Chapter3 Q2 ===================
+
+//=========== Chapter3 Q3 ===================
+// var birthyear = 1996
+// var a = "My birth year is "
+// var b = "Data type of my decelared variable is number"
+// var c = a + birthyear + "\r\n" + b
+// document.write(c)
+
+//=========== Chapter3 Q4 ===================
+// var a = "John Doe"
+// var b = " T-shirt(s)"
+// var c = 5
+// var d = a + " ordered " + c + b + " on XYZ Clothing Store."
+// document.write(d)
+
+//=========== Chapter4 Q1 ===================
+// var a = "a is first variable" , b = "b is second variable" , c= "c is third variable"
+
+//=========== Chapter4 Q2 ===================
+// var myName = "legal variable 1" ;
+// var name1 = "legal variable 2" ;
+// var legalvar = "legal variable 3" ; 
+// var a b = "illegal variable 1";
+// var 123 = "illegal variable 2";
+// var alert = "illegal variable"
+
+
+//=========== Chapter5 Q1 ===================
+// var a = 3
+// var b = 5
+// var c = a + b
+// document.write(c)
+
+//=========== Chapter5 Q2 ===================
+// var a = 3
+// var b = 5
+// var c = b - a
+// document.write(c)
+
+// var x = 3
+// var y = 5
+// var z = x * y
+// document.write(z)
+
+// var i = 3
+// var j = 5
+// var k = i / j
+// document.write (k)
+
+// var m = 3
+// var n = 5
+// var o = n % m
+// document.write(o)
+
+//=========== Chapter5 Q3 ===================
+// var maths = 5
+// document.write ("Value after variable declaration is maths.")
+// var a = ("<br>" + "Initial value is " + maths + "<br>")
+// document.write(a)
+// var b = ++maths
+// document.write("Value after increment is " + b + "." +" <br>")
+// var c = b + 7
+// document.write("Value after addition is " + c + "." + "<br>")
+// var d = --c
+// document.write("Value after decrement is " + d + "." + "<br>")
+// var e = 3
+// var f = d % 3 
+// document.write("The output is " + f + ".")
+
+//=========== Chapter5 Q4 ===================
+// var price = 600
+// var quantity = 5
+// var total = price * quantity
+// document.write ("Total cost of buying 5 tickets to a movie is " + total + "PKR.")
+
+//=========== Chapter5 Q5 ===================
+// for (var i = 1; i <=10 ; i++){
+// document.write ("4" + "x" + i + "=" + 4*i + "<br>") }
+
+//=========== Chapter5 Q6 ===================
+// var ct = 25
+// var f = (ct * 9/5 ) + 32
+// document.write( ct + "\u00B0C is " +  f + "\u00B0F" + "<br>")
+// var ft = 70
+// var c = (ft - 32) * 5/9
+// document.write(ft + "\u00B0F is " + c + "\u00B0C")
+
+//=========== Chapter5 Q7 ===================
+// var a = 650
+// document.write("Price of Item 1 is " + a + "<br>")
+// var b = 3
+// document.write("Quantity of Item 1 is " + b + "<br>" )
+// var c = a * b
+// var d = 100
+// document.write("Price of Item 2 is " + d + "<br>" )
+// var e = 7
+// document.write("Quantity of Item 2 is " + e + "<br>" )
+// var f = d* e
+// var g = 100
+// document.write("Shipping Charges " + g + "<br>")
+// var total = c + f + g
+// document.write("Total cost of your order is " + total + ".")
+
+//=========== Chapter5 Q8 ===================
+// var a = 980
+// document.write("Total marks: " + a + "<br>" )
+// var b = 804
+// document.write("Marks obtained: " + b + "<br>" )
+// var c = b / a * 100
+// document.write("Percentage: " + c)
+
+//=========== Chapter5 Q10 ===================
+// var a = 5, b = a + 5 , c = b * 10, d =c/2
+// document.write(d)
+
+//=========== Chapter6-9 Q1 ===================
+// var a = 10
+// document.write ("The value of a is: " + a + "<br>" + "<br>")
+// var b = ++a
+// document.write ("The value of ++a is: " + b + "<br>")
+// document.write ("Now the value of a is: " + b + "<br>" + "<br>")
+// var c = b++
+// document.write ("The value of a++ is: " + c + "<br>")
+// document.write ("Now the value of a is: " + ++c + "<br>" + "<br>")
+// var d = --c
+// document.write ("The value of --a is: " + d + "<br>")
+// document.write ("Now the value of a is: " + d + "<br>" + "<br>")
+// var e = d--
+// document.write ("The value of a-- is: " + ++d + "<br>")
+// document.write ("Now the value of a is: " + --d + "<br>" + "<br>")
+
+//=========== Chapter6-9 Q2 ===================
+// var a = 2 , b = 1
+// document.write("a is:" + a + "<br>")
+// document.write("b is:" + b + "<br>")
+// var c = --a
+// document.write("--a is:" + c + "<br>")
+// var d = --b
+// var e = c - d
+// document.write("--a - --b is:" + e + "<br>")
+// var f = ++b
+// var g = e + f
+// document.write("--a - --b + ++b is:" + g + "<br>")
+// var h = b--
+// var i = g + h
+// document.write("--a - --b + ++b + b-- is:" + i + "<br>")
+
+//=========== Chapter6-9 Q3 ===================
+// var name = prompt("Your Name")
+// alert("Hi! " + name + ".... Welcome to our website") 
+
+//=========== Chapter6-9 Q5 ===================
+// var input = prompt("Enter any number")
+// for (var  i = 1; i <=10 ; i++)
+// {document.write (input + "x" + i + "=" + input*i + "<br>") }
+
+//=========== Chapter6-9 Q6 ===================
+// var total = 100
+// var sub1 = prompt("Enter 1st subject")
+// var sub2 = prompt("Enter 2st subject")
+// var sub3 = prompt("Enter 3st subject")
+// var marks1 = prompt("Enter obtained marks in subject1")
+// var marks2 = prompt("Enter obtained marks in subject2")
+// var marks3 = prompt("Enter obtained marks in subject3")
+// var per1 = (marks1/total) * 100
+// var per2 = (marks2/total) * 100
+// var per3 = (marks3/total) * 100
+// var totalmarks = total + total + total
+// var obttotal = marks1 + marks2 + marks3
+// var totalper = per1 + per2 + per3
+// document.write( "Subjects" + " " + "Total Marks" + " " + "Marks Obtained" + " " + "Percentage" + "<br>" +
+// sub1 + " " + " " + " " + total + " " + " " + marks1 + " " + " " + per1 + " " + " " + "%" + "<br>" +
+// sub2 + " " + " " + " " + total + " " + " " + marks2 + " " + " " + per2 + " " + " " + "%" + "<br>" + 
+// sub3 + " " + " " + " " + total + " " + " " + marks3 + " " + " " + per3 + " " + " " + "%" + "<br>" +
+// " " + " " + " " + " " + " " + " " + totalmarks +  " " + " " + " " +  " " + " " + " " + obttotal +  " " + " " + " " + " " + " " + " " + totalper)
+
+//=========== Chapter9-11 Q1 ===================
+// var city = prompt("Enter your City")
+// if (city === "karachi"){alert("Welcome to city of lights")}
+
+//=========== Chapter9-11 Q2 ===================
+// var gender = prompt("Enter your gender")
+// if (gender === "male"){alert("Good Morning Sir")}
+// else{alert("Good Morning Ma'am")}
+
+//=========== Chapter9-11 Q3 ===================
+// var light = prompt("Enter any traffic light")
+// if ( light === "red"){alert("Must Stop")}
+// if ( light === "green"){alert("Ready to move")}
+// if ( light === "yellow"){alert("Move now")}
+
+//=========== Chapter9-11 Q4 ===================
+// var fuel = prompt("Fuel in your car (in liters)")
+// if (fuel < 0.25){alert('Please refill the fuel in your car')}
+
+//=========== Chapter9-11 Q5 ===================
+// var a = 4; if (++a === 5){ alert("given condition for variable a is true"); } 
+// var b = 82; if (b++ === 83){ alert("given condition for variable b is true"); }
+// var c = 12; if (c++ === 13){ alert("condition 1 is true"); } if (c === 13){ alert("condition 2 is true"); } if (++c < 14){ alert("condition 3 is true"); } if(c === 14){ alert("condition 4 is true"); } 
+// var materialCost = 20000; var laborCost = 2000; var totalCost = materialCost + laborCost; if (totalCost === laborCost + materialCost){ alert("The cost equals"); }
+// if (true){ alert("True"); } if (false){ alert("False"); } 
+// if("car" < "cat"){ alert("car is smaller than cat"); } 
+
+//=========== Chapter9-11 Q6 ===================
