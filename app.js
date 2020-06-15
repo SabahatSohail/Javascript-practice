bcff11c5f1417934162217103ac97afb5e705f45
SabahatSohail <65030081+SabahatSohail@users.noreply.github.com>
2020-06-15 21:21:23 +0500

    Add files via upload

--- app.js
+++ app.js
@@ -278,3 +278,199 @@
 // if("car" < "cat"){ alert("car is smaller than cat"); } 
 
 //=========== Chapter9-11 Q6 ===================
+// document.write( "<h2>" + "Marks Sheet" + "</h2>")
+
+// var total = 300
+// var mo1 = prompt("Enter marks obtained subject 1")
+// var mo2 = prompt("Enter marks obtained subject 2")
+// var mo3 = prompt("Enter marks obtained subject 3")
+// var totalmarksobt = parseInt(mo1) + parseInt(mo2) + parseInt(mo3)
+// var Percentage = totalmarksobt/total * 100
+// document.write("Total Marks = " + total + "<br>")
+// document.write("Marks Obtained = " + totalmarksobt + "<br>")
+// document.write("Percentage = " + Percentage + "%" + "<br>")
+// if (Percentage >= 80){document.write("Grade: A-one" + "<br>");}
+// if (Percentage >= 70 && Percentage < 80){document.write("Grade: A" + "<br>");}
+// if (Percentage >= 60 && Percentage < 70){document.write("Grade: B" + "<br>")}
+// else if(Percentage < 60){document.write("Grade: Fail" + "<br>")} 
+// if (Percentage >= 80){document.write("Remarks: Excellent");}
+// if (Percentage >= 70 && Percentage < 80){document.write("Remarks: Good");}
+// if (Percentage >= 60 && Percentage < 70){document.write("Remarks: You need to improve")}
+// else if(Percentage < 60){document.write("Remarks: Sorry")}
+
+//=========== Chapter9-11 Q7 ===================
+// var num = 8
+// var guess = prompt("Enter any number from(1-10)")
+// if (parseInt(guess) === num){document.write("Bingo! Correct answer.")}
+// if (parseInt(guess) === 7){document.write("Close enough to the correct answer.")}
+// else{document.write("")}
+
+
+//=========== Chapter9-11 Q8 ===================
+// var num = prompt("Enter any number")
+// var divisible = parseInt(num) % 3
+// if (divisible === 0){document.write("Number is divisible by 3")}
+// else{document.write("Number is not divisible by 3")}
+
+//=========== Chapter9-11 Q9 ===================
+// var num = prompt("Enter any number")
+// var divisible = parseInt(num) % 2
+// if (divisible === 0){document.write("Number is even")}
+// else{document.write("Number is odd")}
+
+
+//=========== Chapter9-11 Q10 ===================
+// var temp = prompt("Enter any temperature")
+// if(temp >= 40){document.write("It is too hot outside.")}
+// if(temp >= 30 && temp < 40){document.write("The Weather today is Normal.")}
+// if(temp >= 20 && temp < 30){document.write("Today’s Weather is cool.")}
+// if(temp > 10 && temp < 20){document.write("OMG! Today’s weather is so Cool.")}
+// else{document.write("")}
+
+
+//=========== Chapter9-11 Q11 ===================
+// var firstnum = prompt("Enter first number")
+// var secondnum = prompt("Enter second number")
+// var operator = prompt("Enter operator")
+// if (operator === "+"){document.write(parseInt(firstnum) + parseInt(secondnum))}
+// if (operator === "-"){document.write(parseInt(firstnum) - parseInt(secondnum))}
+// if (operator === "*"){document.write(parseInt(firstnum) * parseInt(secondnum))}
+// if (operator === "/"){document.write(parseInt(firstnum) / parseInt(secondnum))}
+// if (operator === "%"){document.write(parseInt(firstnum) % parseInt(secondnum))}
+// else{document.write("")}
+
+//=========== Chapter12-13 Q1 ===================
+// var a = prompt("Enter any letter")
+// if (a >="A" && a <= "Z"){document.write(a + " is a Upper Case letter" )}
+// else if (a >="a" && a <= "z"){document.write(a + " is a Lower Case letter" )}
+// else{document.write(a + " is a number")}
+
+//=========== Chapter12-13 Q2 ===================
+// var num1, num2;
+// num1 = prompt("Input the First integer");
+// num2 = prompt("Input the second integer");
+                                                 
+// if(parseInt(num1, 10) > parseInt(num2, 10)) 
+// {document.write("The larger of "+ num1+ " and "+ num2+ " is "+ num1+ ".");}   
+// else if(parseInt(num2, 10) > parseInt(num1, 10))
+// {document.write("The larger of "+ num1+" and "+ num2+ " is "+ num2+ ".");}                  
+// else{document.write("The values "+ num1+ " and "+num2+ " are equal.");}
+
+//=========== Chapter12-13 Q3 ===================
+// var input = prompt("Enter any number")
+// if(parseInt(input) > 0){document.write("Number is positive")}
+// if(parseInt(input) < 0){document.write("Number is negative")}
+// if(parseInt(input) === 0){document.write("Number is zero")}
+// else{document.write("")}
+
+
+//=========== Chapter12-13 Q4 ===================
+// var input = prompt("Enter any letter")
+// var vowels = ["a" , "e", "i", "o", "u"]
+// if(input === vowels[0]){document.write("True")}
+// else if(input === vowels[1]){document.write("True")}
+// else if(input === vowels[2]){document.write("True")}
+// else if(input === vowels[3]){document.write("True")}
+// else if(input === vowels[4]){document.write("True")}
+// else{document.write("False")}
+
+//=========== Chapter12-13 Q5 ===================
+// var password = "password"
+// var input = prompt("")
+// if(input === password){document.write("Correct! The password you entered matches the original password")}
+// else if(input === ""){document.write("Please enter your password")}
+// else{document.write("Incorrect password")}
+
+
+//=========== Chapter12-13 Q6 ===================
+// var greeting; var hour = 19; if (hour < 18)
+// {document.write( greeting = "Good day")}
+// else {document.write (greeting = "Good evening") }
+
+
+//=========== Chapter12-13 Q7 ===================
+
+
+//=========== Chapter14-16 Q1 ===================
+
+
+//=========== Chapter17-20 Q1 ===================
+// var Array = [[],[],[]]
+// document.write(Array[0])
+// document.write(Array[1])
+// document.write(Array[2])
+
+
+//=========== Chapter17-20 Q2 ===================
+// var Arr = [[0,1,2,3], [1,0,1,2], [2,1,0,1]]
+// document.write(Arr[0] + "<br>")
+// document.write(Arr[1] + "<br>")
+// document.write(Arr[2] + "<br>")
+
+
+//=========== Chapter17-20 Q3 ===================
+// for (var a = 1; a <=10; a++)
+// {document.write(a + "<br>")}
+
+//=========== Chapter17-20 Q4 ===================
+// var input = prompt("Enter any number")
+// var length = prompt("Enter length")
+// for (var  i = 1; i <=length ; i++)
+// {document.write (input + "x" + i + "=" + input*i + "<br>") }
+
+//=========== Chapter17-20 Q5 ===================
+// var fruits = ["apple", "banana", "mango", "orange", "strawberry"] 
+// document.write(fruits[0] + "<br>")
+// document.write(fruits[1] + "<br>")
+// document.write(fruits[2] + "<br>")
+// document.write(fruits[3] + "<br>")
+// document.write(fruits[4] + "<br>" + "<br>" + "<br>")
+// document.write("Element at index 0 is " + fruits[0] + "<br>")
+// document.write("Element at index 1 is " + fruits[1] + "<br>")
+// document.write("Element at index 2 is " + fruits[2] + "<br>")
+// document.write("Element at index 3 is " + fruits[3] + "<br>")
+// document.write("Element at index 4 is " + fruits[4] + "<br>")
+
+//=========== Chapter17-20 Q6 ===================
+// var Counting = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+// var Reversecounting = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1] 
+// var Even = [0, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20]
+// var Odd = [1, 3, 5, 7, 9, 11, 13, 15, 17, 19]
+// var Series = ["2k", "4k", "6k", "8k", "10k", "12k", "14k", "16k", "18k", "20k"]
+// document.write(Counting + "<br>")
+// document.write(Reversecounting + "<br>")
+// document.write(Even + "<br>")
+// document.write(Odd + "<br>")
+// document.write(Series + "<br>")
+
+
+//=========== Chapter17-20 Q7 ===================
+// var a = ["cake", "apple pie", "cookie", "chips", "patties"] 
+// var input = prompt("Welcome to ABC Bakery. What do you want to order sir/ma'am?")
+// if (input === a[0]){document.write(input +" is available at index 0 in our bakery" )}
+// else if (input === a[1]){document.write(input +" is available at index 1 in our bakery" )}
+// else if (input === a[2]){document.write(input +" is available at index 2 in our bakery" )}
+// else if (input === a[3]){document.write(input +" is available at index 3 in our bakery" )}
+// else if (input === a[4]){document.write(input +" is available at index 4 in our bakery" )}
+// else{document.write("We are sorry."+ input + " is not available at our bakery.")}
+
+//=========== Chapter17-20 Q8 ===================
+// var arr = [24, 53, 78, 91, 12]
+// document.write(arr + "<br>")
+// var largest = arr[0];
+// for (var i = 0; i < arr. length; i++)
+// { if (largest < arr[i] ) { largest = arr[i]; } }
+// document.write("The largest number in an array is " + largest)
+
+//=========== Chapter17-20 Q9 ===================
+// var arr = [24, 53, 78, 91, 12]
+// document.write(arr + "<br>")
+// var smallest = arr[0];
+// for (var i = 0; i < arr. length; i++)
+// { if (smallest < arr[i-1] ) { smallest = arr[i]; } }
+// document.write("The smallest number in an array is " + smallest)
+
+//=========== Chapter17-20 Q10 ===================
+// for(var i = 1; i<=20; i++)
+// {document.write(5*i + ", ")}
+
